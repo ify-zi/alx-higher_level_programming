66ee5569d297db22d33f681b9a1678104a7b4a19
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-11-28 15:54:13 -0800

    corrections

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(letter):
     checker = ord(letter)
-    if checker >= 97 and chekcker <= 122:
+    if checker >= 97 and checker <= 122:
         return True
     else:
         return False

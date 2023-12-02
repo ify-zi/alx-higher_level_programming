7aacd7fef7d272d77036be3034c7137faad86ed6
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-02 07:26:53 -0800

    pop c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     string_copy = []
-    if not my_string:
-        return None
-    else:
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                string_copy.append(i)
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            string_copy.append(i)
     return ''.join(string_copy)

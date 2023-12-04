3ffe55108b4ce1cee4b8ec711055eb5bc6279aa1
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-04 11:58:21 -0800

    max num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = 0
     if not my_list:
         return None
     else:
-        for i in my_list:
-            if i > max_num:
-                max_num = i
-    return max_num
+        my_list.sort()
+    return my_list[-1]

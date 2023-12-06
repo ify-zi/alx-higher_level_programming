f87f2b273579721b92b92d2b912e44993bcf2c5e
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-06 13:16:30 -0800

    update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary.get(key):
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+    new_dict = {key : value}
+    a_dictionary.update(new_dict)
 
     return a_dictionary

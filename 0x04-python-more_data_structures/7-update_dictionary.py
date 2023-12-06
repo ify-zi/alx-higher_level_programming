cb0fe7e3fd1872a1a4d05e389b0366bbc12e4721
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-06 13:08:25 -0800

    update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary.get(key) is not None:
+    if a_dictionary.get(key):
         a_dictionary[key] = value
     else:
         a_dictionary[key] = value

b7475a6ab773c43c9be32e4c9b8e551565cb0b1b
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-19 04:51:33 -0800

    correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-
     except Exception as te:
         print("Exeption: {}".format(te), file=sys.stderr)
         return False

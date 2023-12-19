4ba0f2f30f11af48674e5672a2afdc196b2012dd
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-19 14:33:39 -0800

    adding an attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defining a class"""
+
+
+class Square:
+    """defining a class square
+
+    Attributes:
+        __size (int): a private attribute of square size
+    """
+    def __init__(self, size):
+        """Initailise the class
+	Args:
+            size (int): the size of the object
+        Return: None
+        """
+        self.__size = size

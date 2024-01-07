3945864cd65b7ba37db46238c30147457377b25c
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-07 12:16:36 -0800

    test

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    lazy_matrix_mul function
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+        returns the product of two matrices
+    """
+    return numpy.matmul(m_a, m_b)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,7 +10,7 @@ def matrix_divided(matrix, div):
     r_len = len(matrix[0])
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix \
-                        (list of lists) of integers/floats")
+(list of lists) of integers/floats")
     for item in matrix:
         if len(item) != r_len:
             raise TypeError("Each row of the matrix must have the same size")

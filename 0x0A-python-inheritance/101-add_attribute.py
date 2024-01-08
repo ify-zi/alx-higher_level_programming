d0961c65adeecbb43ba04f493451e760527a6bd2
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-08 08:26:12 -0800

    setattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,8 +8,6 @@ def add_attribute(a_class, name, value):
     """
         add attributes to a class
     """
-    a_class.name = value
-    if getattr(a_class, name, value):
-        pass
-    else:
+    if not hasattr(a_class, "__dict__"):
         raise TypeError("can't add new attribute")
+    setattr(a_class, name, value)

fd7bf572397e1003d071fcadc751b5b88b82320c
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-09 13:42:28 -0800

    json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,6 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
         create JSON and save to file
     """
-    json_file = json.dumps(my_obj)
     with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(json_file)
+        json.dumps(my_obj, f)

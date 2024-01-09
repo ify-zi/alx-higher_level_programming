7c262b56106364ac3baf4d5d3fc96ad6874cff53
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-09 14:26:14 -0800

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+    Module that returns the dictionary description
+"""
+
+
+def class_to_json(obj):
+    """
+      returns the dictionary description of an obj 
+    """
+
+    my_dict = {}
+    if hasattr(obj, "__dict__"):
+        my_dict = obj.__dict__.copy()
+    return my_dict

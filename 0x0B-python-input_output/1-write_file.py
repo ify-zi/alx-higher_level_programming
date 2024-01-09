fa58a1c075a4d9d96bf4085b6fb683b8442dd0ac
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-09 12:56:23 -0800

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,9 @@
 
 
 def write_file(filename="", text=""):
+    """
+        write to a file or create file
+        if not exist
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)

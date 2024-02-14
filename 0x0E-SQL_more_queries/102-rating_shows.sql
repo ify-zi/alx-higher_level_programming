db997e8be5b69d3faaf3e25ebd100cfc4ad1c674
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-02-14 14:33:03 -0800

    God abeg

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- lists all shows in hbtn_0d_tvshows DB by their rating
+
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
+ORDER BY rating DESC;

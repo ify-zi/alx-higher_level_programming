a6f60128d0f23eb555199f5eb89d8bef0acfc850
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-11 16:59:08 -0700

    factorial correction

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-function factorial(num) {
+function factorial (num) {
   if (num <= 0) {
     return 0;
   } else if (num === 1) {
     return 1;
   } else {
     return num * factorial(num - 1);
-}
+  }
 }
 
 const parNum = parseInt(process.argv[2]);

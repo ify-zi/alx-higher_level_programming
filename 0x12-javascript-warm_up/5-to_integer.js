26abe8d6ec17049cf0f784cb23c64701f6cb81db
Ifeanyi Zion Chidiebere <64507351+ify-zi@users.noreply.github.com>
2024-03-11 17:02:51 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const num = process.argv[2];
 const numc = parseInt(num, 10);
 
-if (isNaN(numc)) {
+if (numc === undefined || isNaN(numc)) {
   console.log('My number: Not a number');
 } else {
   console.log('My number: ' + numc);

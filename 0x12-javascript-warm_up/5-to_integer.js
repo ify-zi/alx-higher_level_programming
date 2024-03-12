93bc087d72fea9acc550c21eaeb133a5a682f9ff
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-11 17:53:47 -0700

    converter

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const num = process.argv[2];
 const numc = parseInt(num, 10);
 
-if (num === undefined || isNaN(numc)) {
+if (process.argv[2] === undefined || isNaN(numc)) {
   console.log('My number: Not a number');
 } else {
   console.log('My number: ' + numc);

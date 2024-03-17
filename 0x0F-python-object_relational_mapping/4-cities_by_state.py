596d01de78291b81df91a961774c1fd05418dea7
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-16 17:02:29 -0700

    5th task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    ORM module - MYSQLdb
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """
+        connectto daabse and retrive a selected data
+    """
+    db_conn = MySQLdb.connect(host="localhost", port=3306,
+                              user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db_conn.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states \
+                ON cities.state_id = states.id \
+                ORDER BY cities.id \
+                ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    db_conn.close()

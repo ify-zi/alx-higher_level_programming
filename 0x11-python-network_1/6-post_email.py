fb456bf021f85c92bbe835653342db489a9f530e
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-28 16:46:30 -0700

    using post request in Request package

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Using the post method on the Requests package
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    link = argv[1]
+    info = {'email': argv[2]}
+    resp = requests.post(link, param=info)
+    print(resp.text)

29a333ed5fc78e4d85e842665d6cb896059add56
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-28 19:15:42 -0700

    using github to get committer in repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    script return users who commited to a repo
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    owner = argv[2]
+    resp = requests.get("https://api.github.com/repos/{}/{}/commits".format(owner, repo))
+    committers = resp.json()
+    for committer in committers[:10]:
+        print(committer.get("sha"), end=": ")
+        print(committer.get('commit').get("author").get("name"))

c196ef4eff483b4736b87b262e6c66ee42d5675c
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-28 17:48:41 -0700

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,12 +10,13 @@ if __name__ == '___main__':
     from sys import argv
 
     t_url = "http://0.0.0.0:5000/search_user"
-    dat = {'q': argv[1][0] if len(argv) > 1}
-    resp = requests.post(t_url, data=dat)
-    f_resp = resp.json()
-    if not f_resp:
-        print("No result")
-    else:
-        print("[{}]: {}".format(f_resp.get('id'), f_resp.get('name')))
+    dat = {'q': argv[1][0] if len(argv) > 1 else ""}
+    resp= requests.post(t_url, data=dat)
+    try:
+        f_resp = resp.json()
+        if not f_resp:
+            print("No result")
+        else:
+            print("[{}]: {}".format(f_resp.get('id'), f_resp.get('name')))
     except ValueError:
         print("Not a valid JSON")

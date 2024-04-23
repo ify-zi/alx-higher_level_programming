ffa6d77cedecb886be6ab1b50a80e71a9dcca804
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-23 15:20:15 -0700

    retriving done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,10 @@ const process = require('process');
 
 const url = process.argv[2];
 
-request(url, function (response) {
-  console.log('code: ', response.statusCode.toString());
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ', response.statusCode.toString());
+  }
 });

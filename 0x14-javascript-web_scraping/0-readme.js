58259ca9bce90d86c6cca2fb1016f506cd65a21b
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-23 06:47:29 -0700

    reading file in javascript

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, msg) {
+	if (err) {
+		console.log(err);
+	} else {
+		process.stdout.write(msg);
+	}
+});

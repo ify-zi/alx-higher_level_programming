a334fe47154cb7cc1c3542b10d75063cf00687f3
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-23 06:51:11 -0700

    read js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const process = require('process');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', function (err, msg) {
-	if (err) {
-		console.log(err);
-	} else {
-		process.stdout.write(msg);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(msg);
+  }
 });
